39298cc83ea9134eb1616ac0251cc18ced32ef42
Alex Britton <alex@ahbritton.com>
2022-01-04 15:24:50 +0000

    first working version

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import Adafruit_PCA9685
 import time
 
+pwm = Adafruit_PCA9685.PCA9685(address=0x40, busnum=1)
+pwm.set_pwm_freq(50)
+
 while 1:
 	for i in range(0,100):
 		pwm.set_pwm(0, 0, (300+i))
