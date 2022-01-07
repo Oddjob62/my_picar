36e9c0539dc349a9afe16372437949f2a8b37b9b
Alex Britton <alex@ahbritton.com>
2022-01-07 23:55:14 +0000

    add more test files

---
+++ README.md
@@ -0,0 +1,4 @@
+# Before running Picar code
+sudo raspi-config # enable I2C interface
+sudo pip3 install Adafruit_PCA9685
+
--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import Adafruit_PCA9685
 import time
+import sys
 
 pwm = Adafruit_PCA9685.PCA9685(address=0x40, busnum=1)
 pwm.set_pwm_freq(50)
 
+servo=int(sys.argv[1])
+
 while 1:
 	for i in range(0,100):
-		pwm.set_pwm(0, 0, (300+i))
-		time.sleep(0.05)
+		pwm.set_pwm(servo, 0, (300+i))
+		time.sleep(0.02)
 	for i in range(0,100):
-		pwm.set_pwm(0, 0, (400-i))
-		time.sleep(0.05)
+		pwm.set_pwm(servo, 0, (400-i))
+		time.sleep(0.02)
